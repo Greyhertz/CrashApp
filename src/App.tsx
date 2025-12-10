f998ee0b11649a238be722283bea55c25c748594
Greyhertz <greyhert120@gmail.com>
2025-12-10 12:50:48 +0100

    Fix: fixed tailwindCss configuraition   issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
+import { Badge } from "lucide-react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Button } from "./components/ui/button";
 import Showcase from "./pages/Showcase";
-
-import ButtonsDemo from "./components/demo/ButtonsDemo";
-import InputsDemo from "./components/demo/InputsDemo";
-import FormsDemo from "./components/demo/FormsDemo";
-import DialogDemo from "./components/demo/DialogDemo";
-import MenusDemo from "./components/demo/MenusDemo";
-import CardsDemo from "./components/demo/CardsDemo";
-import TablesDemo from "./components/demo/TablesDemo";
-import AlertsDemo from "./components/demo/AlertsDemo";
-import TabsDemo from "./components/demo/TabsDemo";
-
+import ButtonsDemo from "./pages/demos/buttondemo";
+import Homepage from "./pages/Homepage";
+// import InputsDemo from "./components/demo/InputsDemo";
+// import FormsDemo from "./components/demo/FormsDemo";
+// import DialogDemo from "./components/demo/DialogDemo";
+// import MenusDemo from "./components/demo/MenusDemo";
+// import CardsDemo from "./components/demo/CardsDemo";
+// import TablesDemo from "./components/demo/TablesDemo";
+// import AlertsDemo from "./components/demo/AlertsDemo";
+// import TabsDemo from "./components/demo/TabsDemo";
+import SidebarDemo from "./pages/demos/Sidebar-demo";
+import DialogDemo from "./pages/demos/Dialog-demo";
+import FormDemo from "./pages/demos/Form-demo";
+import DataDisplayDemo from "./pages/demos/Data-display-demo";
 export default function App() {
-  return (
+  return ( 
+    <div>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Showcase />} />
-        <Route path="/demo/buttons" element={<ButtonsDemo />} />
+        <Routes>
+        <Route path="/" element={<Showcase />} /> 
+          <Route path="/demo/buttons" element={ <ButtonsDemo /> } />
+          <Route path="/demo/sidebar" element={ <SidebarDemo /> } />
+          <Route path="/demo/dialog" element={ <DialogDemo /> } />
+          <Route path="/demo/form" element={ <FormDemo /> } />
+          <Route path="/demo/data-display" element={ <DataDisplayDemo /> } />
+        {/* <Route path="/" element={<Showcase />} />
+        
         <Route path="/demo/inputs" element={<InputsDemo />} />
         <Route path="/demo/forms" element={<FormsDemo />} />
         <Route path="/demo/dialogs" element={<DialogDemo />} />
@@ -24,8 +36,9 @@ export default function App() {
         <Route path="/demo/cards" element={<CardsDemo />} />
         <Route path="/demo/tables" element={<TablesDemo />} />
         <Route path="/demo/alerts" element={<AlertsDemo />} />
-        <Route path="/demo/tabs" element={<TabsDemo />} />
+        <Route path="/demo/tabs" element={<TabsDemo />} /> */}
       </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+   </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App'
 // import './styles/tailwind.css'
 import './App.css'
